a98c47b14947be5645686b8f2e24b42123f368b4
Myrmex <dfusi@hotmail.com>
2023-02-07 12:49:12 +0100

    Refactored infrastructure

--- CadmusVelaApi/Services/VelaPartSeederFactoryProvider.cs
+++ CadmusVelaApi/Services/VelaPartSeederFactoryProvider.cs
@@ -3,12 +3,11 @@ using Cadmus.Seed;
 using Cadmus.Seed.General.Parts;
 using Cadmus.Seed.Philology.Parts;
 using Fusi.Microsoft.Extensions.Configuration.InMemoryJson;
-using SimpleInjector;
 using System;
 using System.Reflection;
-using Microsoft.Extensions.Configuration;
 using Cadmus.Seed.Geo.Parts;
 using Cadmus.Seed.Epigraphy.Parts;
+using Microsoft.Extensions.Hosting;
 
 namespace CadmusVelaApi.Services
 {
@@ -19,16 +18,8 @@ namespace CadmusVelaApi.Services
     public sealed class VelaPartSeederFactoryProvider :
         IPartSeederFactoryProvider
     {
-        /// <summary>
-        /// Gets the part/fragment seeders factory.
-        /// </summary>
-        /// <param name="profile">The profile.</param>
-        /// <returns>Factory.</returns>
-        /// <exception cref="ArgumentNullException">profile</exception>
-        public PartSeederFactory GetFactory(string profile)
+        private static IHost GetHost(string config)
         {
-            if (profile == null) throw new ArgumentNullException(nameof(profile));
-
             // build the tags to types map for parts/fragments
             Assembly[] seedAssemblies = new[]
             {
@@ -44,21 +35,29 @@ namespace CadmusVelaApi.Services
             TagAttributeToTypeMap map = new();
             map.Add(seedAssemblies);
 
-            // build the container for seeders
-            Container container = new();
-            PartSeederFactory.ConfigureServices(
-                container,
-                new StandardPartTypeProvider(map),
-                seedAssemblies);
+            return new HostBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    PartSeederFactory.ConfigureServices(services,
+                        new StandardPartTypeProvider(map),
+                        seedAssemblies);
+                })
+                // extension method from Fusi library
+                .AddInMemoryJson(config)
+                .Build();
+        }
 
-            container.Verify();
+        /// <summary>
+        /// Gets the part/fragment seeders factory.
+        /// </summary>
+        /// <param name="profile">The profile.</param>
+        /// <returns>Factory.</returns>
+        /// <exception cref="ArgumentNullException">profile</exception>
+        public PartSeederFactory GetFactory(string profile)
+        {
+            if (profile == null) throw new ArgumentNullException(nameof(profile));
 
-            // load seed configuration
-            IConfigurationBuilder builder = new ConfigurationBuilder()
-                .AddInMemoryJson(profile);
-            var configuration = builder.Build();
-
-            return new PartSeederFactory(container, configuration);
+            return new PartSeederFactory(GetHost(profile));
         }
     }
 }
